658bf529013559e0d26ad5edebf523edf004b3c4
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2014-12-14 16:10:11 +0100

    Updated CMake configuration of project structure

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,2 +1,2 @@
-SET(CXX_STANDARD_FLAGS "-std=c++11")
+SET(CXX_STANDARD_FLAGS "-Wall -O3 -std=c++1y -fexceptions -g -fexpensive-optimizations -fdevirtualize-speculatively -fdiagnostics-color=always")
 SET(CMAKE_CXX_FLAGS "${CXX_STANDARD_FLAGS}")
--- cmake/FindLibraries.cmake
+++ cmake/FindLibraries.cmake
@@ -1,1 +1,10 @@
 FIND_PACKAGE(GTest REQUIRED)
+FIND_PACKAGE(Doxygen)
+SET(Qt5Widgets_DIR "/opt/Qt5/5.3/gcc_64/lib/cmake/Qt5Widgets/")
+
+# Instruct CMake to run moc automatically when needed.
+SET(CMAKE_AUTOMOC ON)
+# Find includes in corresponding build directories
+SET(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+FIND_PACKAGE(Qt5Widgets REQUIRED)
