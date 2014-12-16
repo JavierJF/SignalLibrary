b28e1dc64f48e66f45768ddc30ed76970a799a34
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2014-12-16 12:18:05 +0100

    Added "Emitter" type
        This type is responsable from launching the signals. The header
        also provides a connect method used to build the connection between
        the Emitter and Delegate types, it also provides the MACROS for a
        "type sugar" interaction.

---
+++ include/emitter.hpp
@@ -0,0 +1,89 @@
+#ifndef EMITTER_HPP
+#define EMITTER_HPP
+
+#include <signal.hpp>
+#include <signal_exceptions.hpp>
+#include <list>
+
+#define _SIGNALS_(...)                                             			\
+    enum SIGNALS {__VA_ARGS__};
+
+#define _CONNECT_(Emitter, SignalName, Delegate, Fun, ...)                  \
+    connect(decltype(Emitter)::SIGNALS::SignalName, Emitter, Delegate, Fun, \
+            ##__VA_ARGS__);
+
+#define _EMIT_(...)                                                         \
+    this->emit<SIGNALS::__VA_ARGS__>();
+
+#define MAX_SIGNALS 1000
+
+class Nothing
+{
+public:
+    void noAction()
+    {
+    }
+};
+
+class Delegate;
+
+class Emitter
+{
+    friend class Delegate;
+private:
+    sep::Signal*            signals;
+    sep::Signal*            noActionSignal;
+    Nothing*                nothing;
+    std::list<Delegate*>*   connectedDelegates;
+
+    void destroy(const int signalName);
+    void remove(Delegate* delegate);
+protected:
+    template<int num>
+    constexpr void emit() const;
+public:
+    Emitter();
+    ~Emitter();
+    template <typename T, typename F, typename... args>
+    void connect(const int signalName, T&& type, F&& f,
+                 args... delegateFunctionArguments);
+};
+
+template<int signalNumber>
+inline constexpr void Emitter::emit() const
+{
+    static_assert(signalNumber <= MAX_SIGNALS,
+                  "Signal number can't be bigger than MAX_SIGNALS.");
+    static_assert(signalNumber >= 0,
+                  "Signal number can't be smaller than zero.");
+    this->signals[signalNumber].launch();
+}
+
+// Necessary to have own template parameters in a function after that, to use
+// template expansion for the indices.
+template <typename T, typename F, typename... args>
+void Emitter::connect(const int signalName, T&& delegate, F&& delegateFunction,
+                      args... delegateFunctionArguments)
+{
+    if (signalName < 0 || signalName > MAX_SIGNALS)
+    {
+        throw std::out_of_range("Not in range signal name.");
+    }
+
+    delegate->addEmitter(this, signalName);
+    this->connectedDelegates->push_back(delegate);
+    this->signals[signalName] = sep::Signal(std::forward<T>(delegate),
+                                            std::forward<F>(delegateFunction),
+                                            std::forward<args>(delegateFunctionArguments)...);
+}
+
+template <typename T, typename F, typename... args>
+void connect(const int signalName, Emitter& emitter, const T& delegate,
+             const F& delegateFunction, args... delegateFunctionArguments)
+{
+    static_assert(std::is_base_of<Delegate,
+                  std::remove_pointer_t<decayedType<T>>>::value, "Not a Delegate!.");
+    emitter.connect(signalName, delegate, delegateFunction, delegateFunctionArguments...);
+}
+
+#endif // EMITTER_HPP
---
+++ src/emitter.cpp
@@ -0,0 +1,47 @@
+#include <emitter.hpp>
+#include <delegate.hpp>
+#include <stdlib.h>
+
+Emitter::Emitter()
+{
+    this->signals            = (sep::Signal*)calloc(MAX_SIGNALS, sizeof(signals));
+    this->noActionSignal     = new sep::Signal(nothing, &Nothing::noAction);
+    this->nothing            = new Nothing();
+    this->connectedDelegates = new std::list<Delegate*>();
+
+    for (int i = 0; i < MAX_SIGNALS; i++)
+    {
+        this->signals[i] = *noActionSignal;
+    }
+}
+
+Emitter::~Emitter()
+{
+    for(auto &delegate : *(this->connectedDelegates))
+    {
+        delegate->removeEmitter(this);
+    }
+
+    free(this->signals);
+    delete(noActionSignal);
+    delete(nothing);
+    delete(connectedDelegates);
+}
+
+void Emitter::destroy(const int signalNumber)
+{
+    if (signalNumber > MAX_SIGNALS)
+    {
+        throw std::out_of_range("Bigger than signals number");
+    }
+    else if (signalNumber < 0)
+    {
+        throw std::out_of_range("Can't be smaller than signals zero");
+    }
+    this->signals[signalNumber] = *noActionSignal;
+}
+
+void Emitter::remove(Delegate* delegate)
+{
+    this->connectedDelegates->remove(delegate);
+}
